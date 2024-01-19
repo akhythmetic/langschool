b6e5d3b35817b4c89e5ab46ac9a00633c8b29b3e
akhythmetic <145756403+akhythmetic@users.noreply.github.com>
2024-01-19 15:22:31 +0100

    tp4 terminé

--- session/enregistrement.php
+++ session/enregistrement.php
@@ -7,6 +7,50 @@
     <title>Enregistrement</title>
     <link rel="stylesheet" href="../styles/style.css">
     <link rel="icon" href="../images/langue.png" type="image/png">
+
+    <?php
+    //#############################################################################################################
+    function enregistrer($n,$p,$adr,$num,$mail,$mdp){
+        /*
+        Role : enregistre la data de l'utilisateur dans la base de donnée
+        Entrée : des chaines de charactere correspondant à l'entré de l'utilisateur
+        Sortie : aucun
+         */
+        //importer le fichier php qui contient la fonction pour se connecter à la bd
+        include '../bd.php';
+        //se connecter à la bd
+        $bdd = getBD();
+        //envoyer la requette
+        try {
+            $rep = $bdd->query("INSERT INTO Clients (nom, prenom, adresse, numero, mail, mdp) VALUES ('".$n."', '".$p."', '".$adr."', '".$num."', '".$mail."', '".$mdp."');");
+        }
+        catch (PDOException $e) {
+            // Handle database query error
+            die("Database error: " . $e->getMessage());
+        }
+    }
+    /*###################################
+    on verifie si tous l'une des variables est vide ou que mdp1 et mdp2 soit différent.
+    si vrai : redirection vers "nouveau.php"
+    si faux : redirection vers "index.php"
+     */
+    $redirind="<meta http-equiv='refresh' content='1;url=../index.php'>"; //balise de redirection vers index
+    $redirnou="<meta http-equiv='refresh' content='1;url=nouveau.php'>"; //balise de redirection vers nouveau
+    $redirnouD="<meta http-equiv='refresh' content='1;url=nouveau.php?"; //balise de redirection vers nouveau sans la fin pour ajouter la donnée de l'utilisateur ultérieurement
+    if(isset($_POST['n']) && isset($_POST['p']) && isset($_POST['adr']) && isset($_POST['num']) && isset($_POST['mail']) && isset($_POST['mdp1']) && isset($_POST['mdp2'])){
+        $flag_mdp=($_POST['mdp1']==$_POST['mdp2']);// drapeau vrai pour mdp1 et mdp2 identique
+        if($_POST['n']!="" && $_POST['p']!="" && $_POST['adr']!="" && $_POST['num']!="" && $_POST['mail']!="" && $_POST['mdp1']!="" && $_POST['mdp2']!="" && $flag_mdp){
+            enregistrer($_POST['n'],$_POST['p'],$_POST['adr'],$_POST['num'],$_POST['mail'],$_POST['mdp1']);//fonction créé plus haut
+            echo $redirind;
+        }
+        else{
+            echo $redirnouD."n=".$_POST['n']."&"."p=".$_POST['p']."&"."adr=".$_POST['adr']."&"."num=".$_POST['num']."&"."mail=".$_POST['mail']."'>";
+        }
+    }
+    else{
+        echo $redirnou;
+    }
+    ?>
 </head>
 
 <body>
@@ -14,20 +58,6 @@
         <a href="../index.php"><img src="../images/langue.png" alt="Logo LangSchool" width="75"></a>
         <h2>Enregistrement</h2>
     </header>
-    <p>
-        <?php
-        echo $_POST['n']."<br>\n";
-        echo $_POST['p']."<br>\n";
-        echo $_POST['adr']."<br>\n";
-        echo $_POST['num']."<br>\n";
-        echo $_POST['mail']."<br>\n";
-        echo $_POST['mdp1']."<br>\n";
-        echo $_POST['mdp2']."<br>\n";
-        ?>
-    </p>
-
-
-
 </body>
 <footer>
         <a href="../index.php">Acceuil</a>
--- session/nouveau.php
+++ session/nouveau.php
@@ -15,27 +15,64 @@
         <h2>Inscription<section>
     </header>
 
+    <?php
+    /*###########################################################################
+    ce code php permet de remettre les données déjà saisies par l'utilisateur lors d'une redirection vers le formulaire
+     */
+    $value="value=";
+    $n="";
+    $p="";
+    $adr="";
+    $num="";
+    $mail="";
+    if(isset($_GET['n'])){
+        if($_GET['n']!=""){
+            $n=$value.$_GET['n'];
+        }
+    }
+    if(isset($_GET['p'])){
+        if($_GET['p']!=""){
+            $p=$value.$_GET['p'];
+        }
+    }
+    if(isset($_GET['adr'])){
+        if($_GET['adr']!=""){
+            $adr=$value.$_GET['adr'];
+        }
+    }    
+    if(isset($_GET['num'])){
+        if($_GET['num']!=""){
+            $num=$value.$_GET['num'];
+        }
+    }
+    if(isset($_GET['mail'])){
+        if($_GET['mail']!=""){
+            $mail=$value.$_GET['mail'];
+        }
+    }
+    ?>
+
     <form action="enregistrement.php" method="POST">
         <label for="n">Nom:</label>
-        <input type="text" id="n" name="n" required><br>
+        <input type="text" id="n" name="n" <?php echo $n ?>><br>
 
         <label for="p">Prénom:</label>
-        <input type="text" id="p" name="p" required><br>
+        <input type="text" id="p" name="p" <?php echo $p ?>><br>
 
         <label for="adr">Adresse:</label>
-        <input type="text" id="adr" name="adr" required><br>
+        <input type="text" id="adr" name="adr" <?php echo $adr ?>><br>
 
         <label for="num">Numéro de téléphone:</label>
-        <input type="text" id="num" name="num" required><br>
+        <input type="text" id="num" name="num" <?php echo $num ?>><br>
 
         <label for="mail">Adresse e-mail:</label>
-        <input type="email" id="mail" name="mail" required><br>
+        <input type="email" id="mail" name="mail" <?php echo $mail ?>><br>
 
         <label for="mdp1">Mot de passe:</label>
-        <input type="password" id="mdp1" name="mdp1" required><br>
+        <input type="password" id="mdp1" name="mdp1"><br>
 
         <label for="mdp2">Confirmer votre mot de passe:</label>
-        <input type="password" id="mdp2" name="mdp2" required><br>
+        <input type="password" id="mdp2" name="mdp2" ><br>
 
         <input type="submit" value="Enregistrer">
     </form>
