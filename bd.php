cbc0508f971327b5689f06504939b6f869a99fbe
akhythmetic <145756403+akhythmetic@users.noreply.github.com>
2024-01-23 11:14:45 +0100

    fct bd

---
+++ bd.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php';
+
+function getBD() {
+    global $localhostCredentials;
+
+    try {
+        $bdd = new PDO(
+            "mysql:host={$localhostCredentials['host']};dbname={$localhostCredentials['dbname']};charset={$localhostCredentials['charset']}",
+            $localhostCredentials['username'],
+        );
+
+        // Configure PDO to throw exceptions on error
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $bdd;
+    } catch (PDOException $e) {
+        // Gérer l'erreur de connexion de la base de données
+        die('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
+}
+?>
